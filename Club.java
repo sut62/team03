3228d8be39c58e9b2752eafd1ce4a46f67d3f4ef
PcFreb <pdcrowd@gmail.com>
2019-12-10 18:02:57 +0700

    Close #57 - แก้code entity หลัก

--- Club.java
+++ Club.java
@@ -24,17 +24,35 @@ import javax.persistence.FetchType;
 @Data
 @Entity
 @NoArgsConstructor
-@Table(name="CLUB")
+@Table(name="Club")
 public class Club {
 
     @Id
     @SequenceGenerator(name="Club_seq",sequenceName="Club_seq")
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator="Club_seq")
-    @Column(name = "CLUB_ID", unique = true, nullable = true)
+    @Column(name = "Club_ID", unique = true, nullable = true)
     private @NonNull Long id;
-}
-
+    private @NonNull String ClubName;
+    private @NonNull String ClubMember;
+    private @NonNull Integer Age;
+    private @NonNull String Objective;
+    private @NonNull String AdvisorstoClub;
+    private @NonNull String ClubType;
+    @Column(name="RENT_DATE")
+    private @NonNull Date rentDate;
    
 
+    @ManyToOne(fetch = FetchType.EAGER, targetEntity = Branch.class)
+    @JoinColumn(name = "Branch_ID", insertable = true)
+    private Branch Branch;
+
+    @ManyToOne(fetch = FetchType.EAGER, targetEntity = Year.class)
+    @JoinColumn(name = "Year_ID", insertable = true)
+    private Year Year;
+
+    @ManyToOne(fetch = FetchType.EAGER, targetEntity = Officer.class)
+    @JoinColumn(name = "Officer_ID", insertable = true)
+    private Officer Officer;
+  
     
 }
