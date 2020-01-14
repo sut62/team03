869a2f0a5d11661f115ef2af207be4a133ea5c5a
Nunthawoot <B5823475@g.sut.ac.th>
2020-01-14 13:46:57 +0700

    ทำ เพิ่มไฟล์ ci.yaml เเละ เพิ่มคำสั่ง test ใน build.gradle และ ลบไฟล์ที่ไม่จำเป็นออก - close #71

--- SE-Sprint1/backend/src/main/java/SE62/team03/Entity/ClubEvent.java
+++ SE-Sprint1/backend/src/main/java/SE62/team03/Entity/ClubEvent.java
@@ -1,6 +1,9 @@
 package SE62.team03.Entity;
 import lombok.*;
 import javax.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
 @Data
 @Entity
 @NoArgsConstructor
@@ -10,25 +13,24 @@ public class ClubEvent {
   @SequenceGenerator(name="ClubEvent_SEQ",sequenceName="ClubEvent_SEQ")
   @GeneratedValue(strategy = GenerationType.SEQUENCE, generator="ClubEvent_SEQ")
   @Column(name="ClubEvent_ID",unique = true, nullable = true)
-  private @NonNull Long clubEventID;
+  @Getter @Setter private @NonNull Long clubEventID;
 
-  private @NonNull String ClubEventName;
-  private @NonNull String ClubEventDate;
-  private @NonNull String ClubHost;
-  private @NonNull long ClubEventPepleAmount;
+  @Getter @Setter private @NonNull String ClubEventName;
+  @Getter @Setter private @NonNull String ClubEventDate;
+  @Getter @Setter private @NonNull String ClubHost;
+  @Getter @Setter private @NonNull long ClubEventPepleAmount;
   
 
   @ManyToOne(fetch = FetchType.EAGER, targetEntity = Clubs.class)
   @JoinColumn(name = "Clubs_ID", insertable = true)
-  private Clubs clubs;
+  @Getter @Setter private Clubs clubs;
 
   @ManyToOne(fetch = FetchType.EAGER, targetEntity = Officer.class)
   @JoinColumn(name = "Officer_ID", insertable = true)
-  private Officer officer;
+  @Getter @Setter private Officer officer;
 
   @ManyToOne(fetch = FetchType.EAGER, targetEntity = Location.class)
   @JoinColumn(name = "Location_ID", insertable = true)
-  private Location location;
-
+  @Getter @Setter private Location location;
 
 }
--- SE-Sprint1/backend/src/main/java/SE62/team03/Repository/OfficerRepository.java
+++ SE-Sprint1/backend/src/main/java/SE62/team03/Repository/OfficerRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import java.util.Collection;
+
 
 @RepositoryRestResource
 public
