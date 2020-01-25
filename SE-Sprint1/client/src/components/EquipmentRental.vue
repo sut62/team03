ff8cac9ad07f8780e9a2301689b3f3f9a08cf7d6
wanrada <creamza_7@hotmail.com>
2020-01-26 01:42:07 +0700

    update UI of equipment and equipmentRental -close #90

--- SE-Sprint1/client/src/components/EquipmentRental.vue
+++ SE-Sprint1/client/src/components/EquipmentRental.vue
@@ -11,7 +11,7 @@
       <v-col cols="4">
         <v-form v-model="valid" ref="form">
           <v-row>
-            <v-col cols="10">
+            <v-col cols="12">
               <v-select
                 label="Clubs"
                 outlined
@@ -25,7 +25,7 @@
             </v-col>
           </v-row>
           <v-row>
-            <v-col cols="10">
+            <v-col cols="12">
               <v-text-field
                 label="Renter name"
                 v-model="equipmentRental.renter_name"
@@ -36,7 +36,7 @@
             </v-col>
           </v-row>
           <v-row>
-            <v-col cols="10">
+            <v-col cols="12">
               <v-select
                 label="Equipment"
                 outlined
@@ -50,7 +50,7 @@
             </v-col>
           </v-row>
           <v-row>
-            <v-col cols="10">
+            <v-col cols="12">
               <v-select
                 label="Officer name"
                 outlined
@@ -63,7 +63,7 @@
           </v-row>
 
           <v-row>
-            <v-col cols="10">
+            <v-col cols="12">
               <v-textarea v-model="equipmentRental.note" auto-grow outlined label="Note" rows="3"></v-textarea>
             </v-col>
           </v-row>
@@ -71,6 +71,7 @@
             <v-col cols="12">
               <v-btn @click="saveEquipmentRental" :class="{ red: !valid, green: valid }">Save</v-btn>
               <v-btn style="margin-left: 15px;" @click="clear">clear</v-btn>
+              <v-btn style="float: right" color="primary" @click="viewData">ดูข้อมูล</v-btn>
             </v-col>
           </v-row>
           <br />
@@ -157,12 +158,12 @@ export default {
           this.equipmentRental
         )
         .then(() => {
+          alert("บันทึกข้อมูลสำเร็จ")
           this.equipmentRental.club_id = ""
           this.equipmentRental.officer_id = ""
           this.equipmentRental.equipment_id = ""
           this.equipmentRental.note = ""
           this.equipmentRental.renter_name = ""
-          this.$router.push("/view")
         })
         .catch(e => {
           console.log(e)
@@ -176,6 +177,9 @@ export default {
       this.getClubs()
       this.getOfficer()
       this.getEquipments()
+    },
+    viewData() {
+      this.$router.push("/viewequipmentrental")
     }
     /* eslint-enable no-console */
   },
--- SE-Sprint1/client/src/components/EquipmentRentalData.vue
+++ SE-Sprint1/client/src/components/EquipmentRentalData.vue
@@ -6,50 +6,80 @@
         <h1 class="display-2 font-weight-bold mb-3">EquipmentRental Information</h1>
       </v-flex>
     </v-layout>
-
+    <v-row justify="center">
+      <v-col cols="6">
+        <v-text-field label="ชื่อชมรม" v-model="clubname" outlined></v-text-field>
+      </v-col>
+      <v-col cols="1">
+        <v-btn style="float: right" x-large color="primary" @click="search">Search</v-btn>
+      </v-col>
+      <v-col cols="1">
+        <v-btn style="float: right" x-large color="primary" @click="reset">Reset</v-btn>
+      </v-col>
+    </v-row>
     <v-row justify="center">
       <v-col cols="8">
-        <v-data-table :headers="headers" :items="items" :items-per-page="5" class="elevation-1"></v-data-table>
+        <v-data-table :headers="headers" :items="tempItem" :items-per-page="5" class="elevation-1"></v-data-table>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
-import http from "../http-common";
+import http from "../http-common"
 export default {
   name: "EquipmentRentalData",
   data() {
     return {
       headers: [
-        { text: "Club room", value: "club.c_name" },
-        { text: "Equipment", value: "equipment.e_name" },
-        { text: "Officer", value: "officerEquipment.o_name" },
-        { text: "Date", value: "equipmentRental_date" },
-        { text: "Renter name", value: "renter_name" },
+        { text: "Equipment Rental ID", value: "equipmentRental_id" },
+        { text: "ชื่อชมรม", value: "clubs.clubName" },
+        { text: "อุบปกรณ์", value: "equipment.e_name" },
+        { text: "เจ้าหน้าที่", value: "officer.name" },
+        { text: "วันที่ยืมอุปกรณ์", value: "equipmentRental_date" },
+        { text: "ผู้ยืมอุปกรณ์", value: "renter_name" },
         { text: "Note", value: "note" }
       ],
-      items: []
-    };
+      items: [],
+      clubname: "",
+      tempItem: [],
+    }
   },
   methods: {
     /* eslint-disable no-console */
+    reset() {
+      this.tempItem = this.items
+    },
+    search() {
+      if(this.clubname === "") {
+        alert("กรุณาใส่ชื่อชมรมเพื่อค้นหา")
+      } else {
+        this.tempItem = []
+        this.items.forEach(club => {
+          if(club.clubs.clubName.includes(this.clubname)) {
+            this.tempItem.push(club)
+          }
+        });
+      }
+    },
     getEquipmentRental() {
       http
         .get("/equipmentrentals")
         .then(response => {
-          this.items = response.data;
+          this.items = response.data
+          this.tempItem = this.items
+          console.log(this.items)
         })
         .catch(e => {
-          console.log(e);
-        });
+          console.log(e)
+        })
     },
     refreshList() {
-      this.getEquipmentRental();
+      this.getEquipmentRental()
     }
   },
   mounted() {
-    this.getEquipmentRental();
+    this.getEquipmentRental()
   }
-};
+}
 </script>
