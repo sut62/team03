cb41de2d6092eac6e7d8974725816ef43eee6765
wanrada <creamza_7@hotmail.com>
2020-01-26 01:21:23 +0700

    update test case of equipment and equipmentRental ลบcode #99

--- SE-Sprint1/backend/src/test/java/SE62/team03/EquipmentTest.java
+++ SE-Sprint1/backend/src/test/java/SE62/team03/EquipmentTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-
 import SE62.team03.Entity.Equipment;
 import SE62.team03.Repository.EquipmentRepository;
 
@@ -13,12 +12,12 @@ import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
 import javax.validation.Validator;
 import javax.validation.ValidatorFactory;
+import java.util.Optional;
 
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
-
+import static org.junit.jupiter.api.Assertions.assertThrows;
 @DataJpaTest
 public class EquipmentTest {
 
@@ -31,6 +30,19 @@ public class EquipmentTest {
     public void setup() {
         ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
         validator = factory.getValidator();
+    }
+
+    @Test
+    void testEquipmentSave() {
+        Equipment equipment = new Equipment();
+        equipment.setE_id(1L);
+        equipment.setE_name("ตู้เก็บของ");
+        equipment = equipmentRepository.saveAndFlush(equipment);
+
+        Optional<Equipment> result = equipmentRepository.findById(equipment.getE_id());
+        
+        assertEquals(1L, result.get().getE_id());
+        assertEquals("ตู้เก็บของ", result.get().getE_name());
     }
 
     @Test
@@ -61,64 +73,5 @@ public class EquipmentTest {
         ConstraintViolation<Equipment> v = result.iterator().next();
         assertEquals("must not be null", v.getMessage());
     }
-
-   
-    // @Test
-    // void testPersonIdOKWith13Digits() {
-    // Person person = new Person();
-    // person.setPersonId("1234567890123");
-
-    // person = personRepository.saveAndFlush(person);
-
-    // Optional<Person> found = personRepository.findById(person.getId());
-    // assertEquals("1234567890123", found.get().getPersonId());
-    // }
-    // @Test
-    // void testPersonIdMustNotBe12Digits() {
-    // Person person = new Person();
-    // person.setPersonId("123456789012"); // 12 digits
-
-    // Set<ConstraintViolation<Person>> result = validator.validate(person);
-
-    // // result ต้องมี error 1 ค่าเท่านั้น
-    // assertEquals(1, result.size());
-
-    // // error message ตรงชนิด และถูก field
-    // ConstraintViolation<Person> v = result.iterator().next();
-    // assertEquals("must match \"\\d{13}\"", v.getMessage());
-    // assertEquals("personId", v.getPropertyPath().toString());
-    // }
-
-    // @Test
-    // void testPersonIdMustNotBe14Digits() {
-    // Person person = new Person();
-    // person.setPersonId("12345678901234"); // 14 digits
-
-    // Set<ConstraintViolation<Person>> result = validator.validate(person);
-
-    // // result ต้องมี error 1 ค่าเท่านั้น
-    // assertEquals(1, result.size());
-
-    // // error message ตรงชนิด และถูก field
-    // ConstraintViolation<Person> v = result.iterator().next();
-    // assertEquals("must match \"\\d{13}\"", v.getMessage());
-    // assertEquals("personId", v.getPropertyPath().toString());
-    // }
-
-    // @Test
-    // void testPersonIdMustBeUnique() {
-    //     // สร้าง person object
-    //     Person p1 = new Person();
-    //     p1.setPersonId("1234567890123");
-    //     personRepository.saveAndFlush(p1);
-
-    //     // คาดหวังว่า DataIntegrityViolationException จะถูก throw
-    //     assertThrows(DataIntegrityViolationException.class, () -> {
-    //         // สร้าง person object ตัวที่ 2
-    //         Person p2 = new Person();
-    //         p2.setPersonId("1234567890123");
-    //         personRepository.saveAndFlush(p2);
-    //     });
-    // }
 
 }
